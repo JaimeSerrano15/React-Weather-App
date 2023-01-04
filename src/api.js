09c1362de3fe8587d1f853f4cd167d33bb09fb97
Jaime Serrano <serranojaime41@gmail.com>
2023-01-03 21:15:27 -0600

    Fetching API information

--- src/api.js
+++ src/api.js
@@ -7,3 +7,6 @@ export const geoAPIOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATRER_API_KEY = "04099bee72a5ca0ed7f25329a811832b"
