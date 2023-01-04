2be36392e503a86b4c20c9165d483a68b25d32e9
Jaime Serrano <serranojaime41@gmail.com>
2023-01-03 20:26:46 -0600

    Initializing project and creating search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5431d64bc4mshb4d1ddb93cb572dp14ff02jsn50650985b08a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
